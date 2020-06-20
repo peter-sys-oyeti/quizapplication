9195f4ebff86bd51ec91a60c9cc0edbd73afafe5
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-20 16:02:46 +0200

    hooked up user on RootStore

--- src/stores/RootStore.js
+++ src/stores/RootStore.js
@@ -2,8 +2,10 @@ import { types } from "mobx-state-tree";
 import { QuizzesStore } from "./QuizzesStore";
 import { QuestionsStore } from "./QuestionsStore";
 import { AnswersStore } from "./AnswersStore";
+import { UsersStore } from "./UsersStore";
 export const RootStore = types.model("RootStore", {
     quizzesStore: types.optional(QuizzesStore, {}),
     questionsStore: types.optional(QuestionsStore, {}),
-    answersStore: types.optional(AnswersStore, {})
+    answersStore: types.optional(AnswersStore, {}),
+    usersStore: types.optional(UsersStore, {})
 });
