77120ab296150fee0da5373e8c07b892decd7476
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-20 16:00:59 +0200

    Edited user store

--- src/stores/UsersStore.js
+++ src/stores/UsersStore.js
@@ -1,42 +1,21 @@
 import { types } from "mobx-state-tree";
 
-export const User = types
-    .model("User", {
-        id: types.string,
-        email: types.string,
-        username: types.string,
-        status: types.string
-    })
-    .views(self => ({
-        get isOnline() {
-            return self.status.includes("online");
-        },
-        get isOffline() {
-            return self.status.includes("offline");
-        }
-    }));
+export const User = types.model("User", {
+    id: types.string,
+    username: types.string
+});
 
 export const UserStore = types
     .model("UserStore", {
         isLoaded: true,
-        users: types.array(User)
+        users: types.array(User),
+        user: types.maybe(User)
     })
-    .views(self => ({
-        get OnlineUsers() {
-            return self.users.filter(user => user.isOnline);
-        },
-        get OnlineUsersCount() {
-            return self.users.filter(user => user.isOnline).length;
-        },
-        get OfflineUsers() {
-            return self.users.filter(user => user.isOffline);
-        },
-        get OfflineUsersCount() {
-            return self.users.filter(user => user.isOffline).length;
-        }
-    }))
     .actions(self => ({
         addUser(user) {
             self.users.push(user);
+        },
+        selectUser(user) {
+            self.user = user;
         }
     }));
