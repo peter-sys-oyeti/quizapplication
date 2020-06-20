e32670b23208d5ebf2a7253b278d6192690bd907
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-20 16:55:14 +0200

    fixed typos

--- src/api/Users.js
+++ src/api/Users.js
@@ -8,13 +8,13 @@ export const saveUser = newUser => {
 
 export const getUser = userName => {
     const Users = InitApi.parse.Object.extend("Users");
-    const users = new InitApi.parse.Query(Usrs);
+    const query = new InitApi.parse.Query(Users);
     query.equalTo("userName", userName);
     return query.find();
 };
 
-export const answersLiveQuery = () => {
-    let query = new InitApi.parse.Query("Answers");
+export const usersLiveQuery = () => {
+    let query = new InitApi.parse.Query("Users");
     let subscription = InitApi.client.subscribe(query);
     return subscription;
 };
